78f3d70e08aaec8d4c3ae880c5e634582a6d13e2
Ali Fakih <afdetails@gmail.com>
2022-05-03 08:56:00 +0200

    Initial Commit

---
+++ Spinner Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spinner Demo
+//
+//  Created by Ali FAKIH on 03/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
